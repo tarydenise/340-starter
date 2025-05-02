60ae3755512b115b93fcb31abb881aa8a04e1636
tarydenise <tarydenise@gmail.com>
2025-05-02 00:07:36 -0700

    new names

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 // Index route
 app.get("/", function(req, res){
-  res.render("index", {title: "Express Routes"})
+  res.render("index", {title: "Home"})
 })
 
 /* ***********************
