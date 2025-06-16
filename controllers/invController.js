5ee9bfe4d39e394da967acebb0dc7d33f7813066
tarydenise <tarydenise@gmail.com>
2025-06-16 03:18:25 -0700

    W08 Task One

--- controllers/invController.js
+++ controllers/invController.js
@@ -1,23 +1,23 @@
-const invModel = require("../models/inventory-model")
-const utilities = require("../utilities/")
+const invModel = require("../models/inventory-model");
+const utilities = require("../utilities/");
 
-const invCont = {}
+const invCont = {};
 
 /* ***************************
  *  Build inventory by classification view
  * ************************** */
 invCont.buildByClassificationId = async function (req, res, next) {
-  const classification_id = req.params.classificationId
-  const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
-  let nav = await utilities.getNav()
-  const className = data[0].classification_name
+  const classification_id = req.params.classificationId;
+  const data = await invModel.getInventoryByClassificationId(classification_id);
+  const grid = await utilities.buildClassificationGrid(data);
+  let nav = await utilities.getNav();
+  const className = data[0].classification_name;
   res.render("./inventory/classification", {
     title: className + " vehicles",
     nav,
     grid,
-  })
-}
+  });
+};
 
 /* ***************************
  *  Build inventory by classification view
@@ -37,7 +37,37 @@ invCont.buildByVehicleId = async function (req, res, next) {
   res.render("./inventory/vehicle", {
     title: `${data.inv_year} ${data.inv_make} ${data.inv_model}`,
     nav,
-    vehicle: data, 
+    vehicle: data,
+  });
+};
+
+// Management View
+invCont.buildManagement = async function (req, res, next) {
+  let nav = await utilities.getNav();
+  res.render("inventory/management", {
+    title: "Management",
+    nav,
+    errors: null,
+  });
+};
+
+//Build ADD NEW CLASSIFICATION view
+invCont.buildAddClassification = async (req, res) => {
+  let nav = await utilities.getNav();
+  res.render("inventory/add-classification", {
+    title: "Add New Classification",
+    nav,
+    errors: null,
+  });
+};
+
+//Build ADD NEW INVENTORY view
+invCont.buildAddInventory = async (req, res) => {
+  let nav = await utilities.getNav();
+  res.render("inventory/add-inventory", {
+    title: "Add New Inventory",
+    nav,
+    errors: null,
   });
 };
 
@@ -46,6 +76,6 @@ invCont.buildByVehicleId = async function (req, res, next) {
  * ************************** */
 invCont.triggerError = (req, res, next) => {
   throw new Error("This is an intentional 500 error for testing.");
-} 
+};
 
 module.exports = invCont;
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,15 +1,26 @@
-// Needed Resources 
-const express = require("express")
-const router = new express.Router() 
-const invController = require("../controllers/invController")
+// Needed Resources
+const express = require("express");
+const router = new express.Router();
+const invController = require("../controllers/invController");
 const utilities = require("../utilities/");
 
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 
 // Route to vehicle detail page
-router.get("/detail/:invId", utilities.handleErrors(invController.buildByVehicleId)
+router.get(
+  "/detail/:invId",
+  utilities.handleErrors(invController.buildByVehicleId)
 );
+
+// Route to build the inventory management view
+router.get("/", invController.buildManagement);
+
+// Route to display the add classification form
+router.get("/add-classification", invController.buildAddClassification);
+
+// Route to display the add inventory form
+router.get("/add-inventory", invController.buildAddInventory);
 
 // Route to trigger an error for testing purposes
 router.get("/error-test", utilities.handleErrors(invController.triggerError));
